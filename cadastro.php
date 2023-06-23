d756be11f9a535afba28957ab37bfbecb47a437c
lndsilva <lndsilva@hotmail.com>
2023-06-23 10:44:17 -0300

    projeto web tipi01

---
+++ cadastro.php
@@ -0,0 +1,10 @@
+<?php
+    $nome = $_GET['nome'];
+    if($nome == 'Senac'){
+        echo "Olá";
+    }else{
+        echo "Error";
+    }
+
+?>
+
---
+++ js/script.js
@@ -0,0 +1,5 @@
+leandro = 'Senac'
+leandro = 10;
+leandro = true 
+
+console.log(leandro);
